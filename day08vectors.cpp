ff76e9c0f2396262d8eef4564fbab8b5df23b3f9
Sadeem Fatima <sadeemfatima2006@gmail.com>
2026-02-18 20:08:54 +0500

    Day#08: Subarray Question

---
+++ day08vectors.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//SubArray Question
+int main(){
+int n=5;
+int arr[]={1,2,3,4,5};
+
+for(int st=0; st<n; st++){
+    for(int end=st; end<n; end++){
+        for(int i=st; i<=end; i++){
+            cout << arr[i];
+        }
+        cout << " ";
+    }
+    cout<< endl;
+}
+    
+
+  
+    return 0;
+
+}
+
+
+
